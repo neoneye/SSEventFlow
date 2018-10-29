02c771ef76329a423eac81aecbfd9ddbe9d51312
Simon Strandgaard <neoneye@gmail.com>
2018-10-29 15:37:58 +0000

    updated copyright year

--- SSEventFlow_Framework/SSEventFlow.h
+++ SSEventFlow_Framework/SSEventFlow.h
@@ -1,4 +1,4 @@
-// MIT license. Copyright (c) 2017 Simon Strandgaard. All rights reserved.
+// MIT license. Copyright (c) 2018 Simon Strandgaard. All rights reserved.
 #import <UIKit/UIKit.h>
 
 //! Project version number for SSEventFlow.
